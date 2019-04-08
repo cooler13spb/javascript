0590e99b6df4e1debf8426df5c6f2333a7d5b5de
cooler13spb <web13spb@yandex.ru>
2019-04-08 23:01:38 +0300

    3hw

--- hw3/js/script3.js
+++ hw3/js/script3.js
@@ -1,6 +1,6 @@
 console.log("Задание 1:");
 let arr = [1, 2, 3, 4, 5];
-let arr2 = [1, 2, 3, 4, 5];
+let arr2 = [1, 2, 9, 4, 5];
 
 function comp(a, a2){
 if (a.length === a2.length){
@@ -8,16 +8,13 @@ for (let i = 0; i < a.length; i++){
 	if (a[i] === a2[i]){
 	}
 		else {
-			res = "folse";
-			return res;
+			return false;
 		}
 	}
 } else {
-	res = "folse"
-	return res;
+	return false;
 }
-res = "true";
-return res;
+return true;
 }
 
 res = comp(arr, arr2);
@@ -43,8 +40,21 @@ console.log("Задание 3:");
 
 function range(a, b, c) {
   let arr = [];  
+  if (!c) c = 1;
   for (let i = a; i <= b; i += c) arr.push(i);
   return arr;
 }
 
 console.log(range(2, 15, 3));
+
+console.log("Задание 4:");
+
+function countProduct(count) {
+	let product = count+" товаров";
+    if (count % 100 < 5 || count % 100 > 20) {
+        if (count % 10 == 1) product = count+" товар";
+        if (count % 10 > 1 && count % 10 < 5) product = count+" товара";
+    }
+    return product;
+}
+console.log(countProduct(3));
