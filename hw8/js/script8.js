0c873f3623158b58bc83ded0cdeac43e15ba1a8a
cooler13spb <web13spb@yandex.ru>
2019-04-16 00:06:11 +0300

    8hw

---
+++ hw8/js/script8.js
@@ -0,0 +1,50 @@
+let comment = document.getElementsByName('comment');
+
+
+let input = document.getElementById('input');
+
+
+input.addEventListener('click', inText);
+
+//проверку на заполнение формы не успел сделать:(
+
+function inText(){
+	var arrImg = ["img/1.png", "img/3.png", "img/2.png"];
+	var randImg = Math.floor(Math.random() * arrImg.length);
+
+	var arrName = ["Дмитрий", "Леонид", "Василий", "Анатолий"];
+	var randName = Math.floor(Math.random() * arrName.length);
+
+
+	let box = document.createElement('div');
+	box.classList.add('box');
+	post.appendChild(box);
+
+	let img = document.createElement('img');
+	img.setAttribute('src', arrImg[randImg]);
+	img.setAttribute('id', 'img');
+	img.classList.add('img');
+	box.appendChild(img);
+
+
+	let name = document.createElement('div');
+	name.classList.add('name');
+	let nameT = arrName[randName];
+	name.appendChild(document.createTextNode(nameT));
+	box.appendChild(name);
+	
+
+	let data = document.createElement('em');
+	data.classList.add('data');
+	let date = Date();
+	data.appendChild(document.createTextNode(date));
+	box.appendChild(data);
+
+
+	let text = document.getElementById('comment').value;
+
+	let com = document.createElement('div');
+	com.classList.add('com');
+	box.appendChild(com);
+	com.innerHTML = text;
+}
